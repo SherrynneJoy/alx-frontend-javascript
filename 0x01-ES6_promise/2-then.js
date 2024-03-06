92f02a13a4bcc048b9b58d378f9bd4d97075d5ba
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-03-06 10:13:44 +0000

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,7 @@ export default function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return ({ status: 200, body: 'Success' });
   }).catch((rejectionReason) => {
+    console.log('Got a response from the API');
     return (new Error());
   });
 }
