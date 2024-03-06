df9400156834b80746579f35a16205b58608a7ec
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-03-06 14:59:03 +0000

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,5 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    if (filename) {
-      reject(new Error('$fileName cannot be processed'));
-    }
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
