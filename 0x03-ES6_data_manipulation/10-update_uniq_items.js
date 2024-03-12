2e5c099569dcc8839a5b7013b95d3a03cad96364
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-03-12 14:52:55 +0000

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(myMap) {
   if (!(myMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-  myMap.forEach((value, key, map) => {
+  myMap.forEach((value, key) => {
     if (value === 1) {
       myMap.set(key, 100);
     }
