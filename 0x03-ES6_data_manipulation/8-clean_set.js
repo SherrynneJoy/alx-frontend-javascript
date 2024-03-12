bdc14a1857ac9dd070b79c83e8ac478fbc692da2
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-03-12 13:48:50 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return Array.from(set).join('-');
+  }
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
+}
